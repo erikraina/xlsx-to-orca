9089f1255e914c5180552fbd31fc2849be6b3695
Erik Raina <rainaerik12@gmail.com>
2022-12-22 21:30:05 -0800

    12/22/22

---
+++ index.js
@@ -0,0 +1,8 @@
+function fileInput() {
+    const userFile = document.querySelector("#userInput")
+    console.log(userFile);
+    console.log("working function")
+    return console.log("yay")
+};
+
+console.log(fileInput);
